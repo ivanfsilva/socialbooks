a2b7802a589f0e0eee4433b45dc8f9561c3494ed
ivanfsilva <ivanfs27@gmail.com>
2019-10-24 10:32:28 -0300

    adicionado Spring Security e cache na busca por autor

--- src/main/java/br/com/ivanfsilva/socialbooks/resources/AutoresResource.java
+++ src/main/java/br/com/ivanfsilva/socialbooks/resources/AutoresResource.java
@@ -2,11 +2,14 @@ package br.com.ivanfsilva.socialbooks.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +28,9 @@ public class AutoresResource {
 	@Autowired
 	private AutoresService autoresService;
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, produces = {
+			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE
+	})
 	public ResponseEntity<List<Autor>> listar() {
 		List<Autor> autores = autoresService.listar();
 		return ResponseEntity.status(HttpStatus.OK).body(autores);
@@ -44,7 +49,10 @@ public class AutoresResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Autor> buscar(@PathVariable("id") Long id) {
 		Autor autor = autoresService.buscar(id);
-		return ResponseEntity.status(HttpStatus.OK).body(autor);
+		
+		CacheControl cacheControl = CacheControl.maxAge(20, TimeUnit.SECONDS);
+		
+		return ResponseEntity.status(HttpStatus.OK).cacheControl(cacheControl).body(autor);
 	}
 
 }
